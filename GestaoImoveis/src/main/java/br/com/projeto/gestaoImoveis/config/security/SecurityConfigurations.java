8216d2bf3e8eb3d39436daadb5a82bf44df43ea2
wellingtonolive <wellingtoncarvalho11@gmail.com>
2020-11-10 23:16:35 -0300

    Configurando Segurança

---
+++ GestaoImoveis/src/main/java/br/com/projeto/gestaoImoveis/config/security/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package br.com.projeto.gestaoImoveis.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+}
